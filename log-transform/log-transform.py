a75ecbe90b3f9cbca3065ffbd2dd27a78bd61d4d
MOHIT KUMAR <111651529+mahit8171@users.noreply.github.com>
2026-06-08 04:24:28 +0530

    Solve log-transform

--- log-transform/log-transform.py
+++ log-transform/log-transform.py
@@ -4,4 +4,4 @@ def log_transform(values):
     """
     Apply the log1p transformation to each value.
     """
-    return [math.log1p(x) for x in values]
+    return [float(math.log1p(v)) for v in values]
