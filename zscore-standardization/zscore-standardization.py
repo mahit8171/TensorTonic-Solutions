2cb5182cd7c1b365e9fbee943ab4ffc69015cc8d
MOHIT KUMAR <111651529+mahit8171@users.noreply.github.com>
2026-06-08 04:26:31 +0530

    Solve zscore-standardization

--- zscore-standardization/zscore-standardization.py
+++ zscore-standardization/zscore-standardization.py
@@ -6,8 +6,8 @@ def zscore_standardize(X, axis=0, eps=1e-12):
     Return np.ndarray (float).
     """
     X = np.asarray(X, dtype=float)
-    
+
     mean = np.mean(X, axis=axis, keepdims=True)
     std = np.std(X, axis=axis, keepdims=True)
-    
+
     return (X - mean) / (std + eps)
